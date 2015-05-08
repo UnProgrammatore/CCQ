0109bc3123ffa4b80900f1dc41b753f99f7ac577
UnProgrammatore <andrea.appstore@gmail.com>
2015-05-08 10:18:09 +0200

    Aggiunto algoritmo banale per la fattorizzazione con mpi

---
+++ provaMpiFattorizzaEasy/fattor.c
@@ -0,0 +1,108 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct elem { // Very basic and non-reusable stack
+	long long val;
+	struct elem* next;
+};
+
+void add(struct elem** head, long long val) {
+	struct elem* app = malloc(sizeof(struct elem));
+	app->val = val;
+	app->next = *head;
+	*head = app;
+}
+
+long long pick(struct elem** head) {
+	long long toret;
+	struct elem* app;
+	if(*head == NULL)
+		return 0;
+	else {
+		toret = (*head)->val;
+		app = *head;
+		*head = (*head)->next;
+		free(app);
+		return toret;
+	}
+}
+
+void master_procedure(int comm_size) {
+	int i = 1;
+	long long rec;
+	int shit_happened;
+	
+	while(i < comm_size) {
+		shit_happened = MPI_Recv(&rec, 1, MPI_LONG_LONG, i, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		
+		if(shit_happened) {
+			fprintf(stderr, "Recv failed");
+			MPI_Abort(MPI_COMM_WORLD, 1);
+		}
+
+		//fprintf(stderr, "Received: %lld\n", rec);
+		if(rec == 0)
+			++i;
+		else
+			printf("Factor: %lld\n", rec);
+	}
+}
+
+void slave_procedure(int my_rank, int comm_size, long long the_number) {
+	long long from, to;
+	long long to_send;
+	int shit_happened;
+	struct elem* head = NULL;
+
+	from = (the_number / (2 * comm_size)) * (my_rank - 1);
+	to = (the_number / (2 * comm_size)) * my_rank;
+
+	from = from == 0 ? 1 : from;
+
+	while(from < to) {
+		//fprintf(stderr, "Trying: %lld\n", from);
+		if(the_number % from == 0) {
+			//fprintf(stderr, "Found: %lld\n", from);
+			add(&head, from);
+		}
+		++from;
+	}
+
+	do {
+		to_send = pick(&head);
+		shit_happened = MPI_Send(&to_send, 1, MPI_LONG_LONG, 0, 0, MPI_COMM_WORLD);
+		//fprintf(stderr, "I'm %d and i'm sending %lld\n", my_rank, to_send);
+
+		if(shit_happened) {
+			fprintf(stderr, "Send failed");
+			MPI_Abort(MPI_COMM_WORLD, 1);
+		}
+
+	}while(to_send != 0);
+}
+
+int main(int argc, char** argv) {
+	int my_rank, comm_size;
+	long long the_number;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+	if(argc <= 1) {
+		fprintf(stderr, "Missing number as argument");
+		MPI_Abort(MPI_COMM_WORLD, 1);
+	}
+	else
+		the_number = atoll(argv[1]);
+	//fprintf(stderr, "the_number is: %lld\n", the_number);
+
+	if(my_rank == 0)
+		master_procedure(comm_size);
+	else
+		slave_procedure(my_rank, comm_size, the_number);
+
+	MPI_Finalize();
+	return 0;
+}
