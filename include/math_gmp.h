b8bef406664cb092802589d7e149f4052cd64b71
UnProgrammatore <andrea.appstore@gmail.com>
2015-05-20 17:52:29 +0200

    Tolto mcd

--- include/math_gmp.h
+++ include/math_gmp.h
@@ -3,7 +3,6 @@
 
 #include <gmp.h>
 
-void mcd(mpz_t output, mpz_t in1, mpz_t in2);
 void mul_mod_n(mpz_t output, mpz_t mul1, mpz_t mul2, mpz_t mod);
 void pow_mod_n(mpz_t output, mpz_t base, mpz_t exp, mpz_t mod);
 
