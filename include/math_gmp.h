befc693e30ef48775d918615cfd9f54dd0d11f73
UnProgrammatore <andrea.appstore@gmail.com>
2015-05-20 18:02:56 +0200

    Tolto tutto quello che abbiamo appena fatto, perchè esiste già

--- include/math_gmp.h
+++
@@ -1,9 +0,0 @@
-#ifndef MATH_GMP_GUARD
-#define MATH_GMP_GUARD
-
-#include <gmp.h>
-
-void mul_mod_n(mpz_t output, mpz_t mul1, mpz_t mul2, mpz_t mod);
-void pow_mod_n(mpz_t output, mpz_t base, mpz_t exp, mpz_t mod);
-
-#endif MATH_GMP_GUARD
