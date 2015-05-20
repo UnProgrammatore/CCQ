266d676b8697ea4ea43ed1a8eba4d765e4de7ea1
UnProgrammatore <andrea.appstore@gmail.com>
2015-05-20 17:40:32 +0200

    Fatto header

--- include/math_gmp.h
+++ include/math_gmp.h
@@ -3,6 +3,8 @@
 
 #include <gmp.h>
 
-void mcd(mpz_t, mpz_t, *mpz_t);
+void mcd(mpz_t output, mpz_t in1, mpz_t in2);
+void mul_mod_n(mpz_t output, mpz_t mul1, mpz_t mul2, mpz_t mod);
+void pow_mod_n(mpz_t output, mpz_t base, mpz_t exp, mpz_t mod);
 
 #endif MATH_GMP_GUARD
