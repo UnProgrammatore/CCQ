893df8ee641b719a3dc9eccf1eb8e9ea7a7e4968
UnProgrammatore <andrea.appstore@gmail.com>
2015-05-30 14:58:13 +0200

    2 o 3 commenti, cose da togliere

--- include/sieve.h
+++ include/sieve.h
@@ -4,17 +4,17 @@
 // Aggiungere l'header con le pair
 
 void sieve(
-	mpz_t n,
-	unsigned int* factor_base,
-	unsigned int base_dim,
-	pair* solutions,
-	unsigned int** exponents,
-	unsigned int** exp_mod2,
-	unsigned int* first_bit_1,
-	unsigned int* count_1,
-	unsigned int* valori_a,
-	unsigned int* dependencies,
-	unsigned int poly_val_num
+	mpz_t n, // Il numero da fattorizzare
+	unsigned int* factor_base, // La base di fattori
+	unsigned int base_dim, // La dimensione della base di fattori
+	pair* solutions, // Il vettore contenente le soluzioni all'equazione
+	unsigned int** exponents, // Il vettore di vettori degli esponenti
+	unsigned int** exp_mod2, // PROBABILMENTE REMOVIBILE: vettore di vettori degli esponenti modulo 2
+	unsigned int* first_bit_1, // PROBABILMENTE REMOVIBILE: vettore delle posizioni dei primi bit a 1
+	unsigned int* count_1, // PROBABILMENTE REMOVIBILE: vettore dei numeri di uni nel vettore mod2
+	unsigned int* valori_a, // I valori di A
+	unsigned int* dependencies, // FORSE REMOVIBILE: Le dipendenze lineari
+	unsigned int poly_val_num // Il numero di valori di A che si vogliono provare nel polinomio
 	);
 
 #endif // SIEVE_GUARD
