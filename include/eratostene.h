69f2e20ffe3707c8363bfe6ded74e9aa09745339
andrea-segalini <segalini.ndr@gmail.com>
2015-05-30 12:11:06 +0200

    Aggiunto in "src" il file "eratostene.c" e in "include" il file "eratostene.h".

---
+++ include/eratostene.h
@@ -0,0 +1,20 @@
+/* Crivello di eratostene parallelo (openmp).
+ * Parametri:
+ * 	"int * sieve": array di int di lunghezza almeno n/2 opportunamente
+ *  	allocato dal chiamante nel quale saranno marchiati con "1" i
+ * 		numeri dispari primi. NOTA BENE: "sieve[i] = 1" indica che il
+ * 		numero "(i * 2) + 1" è primo (sieve contiene solo numeri dispari)
+ *  "long unsigned n": numero fino al quale si desidera calcolare i primi
+ *  Compilazione:
+ * 		$ gcc -c -lgmp -fopenmp -o eratostene.o eratostene.c
+ */
+
+#ifndef ERATOSTENE_H
+#define ERATOSTENE_H 1
+
+#include <stdlib.h>
+#include <omp.h>
+
+void eratosthenes_sieve(unsigned int * sieve, long unsigned n);
+
+#endif // ERATOSTENE_H
