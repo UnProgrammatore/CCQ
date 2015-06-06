c5aa417b3ecae6e5aef6c4b0293c7367b872209c
pattapatta <lorenzo.pattarini@studenti.unipr.it>
2015-06-06 15:50:03 +0200

    sistemato legendre

--- src/legendre.c
+++ src/legendre.c
@@ -35,18 +35,31 @@
 
 
 
-/*
+
 void inizializza_pair(pair p){
   p.sol1=0;
   p.sol2=0;
 }
-*/
+
+
+
+int legendre(mpz_t n, unsigned int p ){
+
+
+int sol;
+mpz_t m;
+mpz_init(m)
+mpz_mod_ui(m,n,p); // m = n % p;
+
+  for (unsigned j = 0; 2 * j < p ; ++j){
+    if ((j*j) % p == m) {
+      return 1;
+    }
+  }
+
+}
 
 /*
-	Calcola (n|p).
-	s=sqrt(N)
-	in solution metto le soluzioni dell'equazione x^2=n mod p
-*/
 int legendre(mpz_t n, unsigned int p ){
 
   p = (p-1)/2;   // sono sicuro sia intero
@@ -65,8 +78,56 @@ int legendre(mpz_t n, unsigned int p ){
   return -1;
 
 }
+*/
 
 
+
+/*
+  Calcola (n|p).
+  s=sqrt(N)
+  in solution metto le soluzioni dell'equazione x^2=n mod p
+*/
+
+
+int legendre_sol(mpz_t n, unsigned int p, pair sol ){
+ 
+  int leg = 0;
+  /*
+    Parte del prof per la ricerca delle soluzioni di 
+     x^2=a mod p
+  */
+  mpz_t m; //     m = n % p;
+  mpz_init(m);
+  mpz_mod_ui(m,n,p);
+
+  mpz_t s;
+  mpz_init(s);
+  mpz_sqrt(s,n);
+
+
+    // Data la simmetria delle soluzioni di "x^2 = n mod p" quando "p"
+    // è dispari, è sufficiente esaminare i valori di "x"
+    // nell'intervallo [0, (p - 1) / 2]
+    for (unsigned j = 0; 2 * j < p ; ++j){
+      if ((j*j) % p == m) {
+      leg = 1;  
+
+      // Se si entra qui vuol dire che "n" è un residuo quadratico
+      // modulo "p", e quindi "p" fa parte della "base di fattori".
+      // Le due soluzioni di "Q(A) = n mod p" si possono calcolare a
+      // partire dalle soluzioni di "x^2 = n mod p".  Bisogna
+      // accertarsi che la soluzione calcolata cada nell'intervallo
+      // [0, p - 1], e per questo sono necessarie le operazioni di
+      // modulo.
+      sol.sol1 = (p - ((s - j) % p)) % p;
+      sol.sol2 = (p - ((j + s) % p)) % p;
+
+      return leg;
+   }
+}
+
+
+}
 
 /*3
 int main(){
